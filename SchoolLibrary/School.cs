6bb3d0ac0822562c9e4813b315f536d082790fe4
jcarstensen <jan.carstensen@buchner.de>
2021-10-12 20:31:44 +0200

    initial commit

---
+++ SchoolLibrary/School.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public class School
+    {
+        string SchoolName;
+        string SchoolAdress;
+        string SchoolCity;
+        string SchoolState;
+        string SchoolZip;
+        string Phonenumber;
+
+        Elementary.Volume volume;
+        HighSchool.Volume volumeAdvanced;
+
+
+    }
+}
